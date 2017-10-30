7f76b50d955734e961caa3189580a1da1258ecfb
gyy <804798382@qq.com>
2017-10-30 21:31:17 +0800

    suchapc

--- pc.sucha.com/js/global.js
+++ pc.sucha.com/js/global.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     /*导航*/
-    $('.userSe').hover(function(){
-        $(this).addClass('cur');
-        $('.userSe dd').show();
-    },function(){
-        $(this).removeClass('cur');
-        $('.userSe dd').hide();
-    });
+    // $('.userSe').hover(function(){
+    //     $(this).addClass('cur');
+    //     $('.userSe dd').show();
+    // },function(){
+    //     $(this).removeClass('cur');
+    //     $('.userSe dd').hide();
+    // });
+
+    $('#tel').html('400-666-1012');
 
 });
